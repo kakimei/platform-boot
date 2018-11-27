a3c15abeb062586b576e221c9d8e712747500d48
yiminghe <yiming.he@coupang.com>
2018-11-27 12:20:25 +0800

    change

--- src/main/java/com/platform/feedback/controller/FeedBackController.java
+++ src/main/java/com/platform/feedback/controller/FeedBackController.java
@@ -4,13 +4,13 @@ import com.platform.facade.Request;
 import com.platform.feedback.controller.vo.FeedBackVO;
 import com.platform.feedback.facade.FeedBackFacade;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping(value = "/feedback")
 public class FeedBackController {
 
--- src/main/java/com/platform/reserve/controller/ReserveController.java
+++ src/main/java/com/platform/reserve/controller/ReserveController.java
@@ -7,14 +7,14 @@ import com.platform.reserve.facade.ReserveFacade;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@Controller
+@RestController
 @RequestMapping(path = "/reserve")
 public class ReserveController {
 
