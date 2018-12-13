f2d1dfd3863e683deb86a9f8d3652ce656c24c14
yiminghe <yiming.he@coupang.com>
2018-12-14 02:10:43 +0800

    change

---
+++ src/main/java/com/platform/BoFilter.java
@@ -0,0 +1,59 @@
+package com.platform;
+
+import com.platform.bo.userinfo.service.BoUserService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Slf4j
+@Component
+@WebFilter(urlPatterns = "/bo/*", filterName = "boFilter")
+@Order(3)
+public class BoFilter implements Filter {
+
+	private static final String LOGIN_URI = "/platform/bo/login";
+
+	@Autowired
+	private BoUserService boUserService;
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+		throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest)servletRequest;
+		if(LOGIN_URI.equals(request.getRequestURI())){
+			filterChain.doFilter(servletRequest, servletResponse);
+			return;
+		}
+		String boUserToken = request.getParameter("boUserToken");
+		String boUserName = request.getParameter("boUser");
+		String boUser = boUserService.check(boUserToken, boUserName);
+		if(StringUtils.isNotBlank(boUser)){
+			request.setAttribute("boUser", boUser);
+			filterChain.doFilter(servletRequest, servletResponse);
+		}else{
+			throw new ServletException("bo user login failed or expired.");
+		}
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+}
